d7cfab850d3958ddd11608f80304e927e7dbca6d
latypova-alina <alinkin9696@mail.ru>
2017-10-27 18:12:24 +0300

    add uid to device

--- app/api/device/device_api.rb
+++ app/api/device/device_api.rb
@@ -11,6 +11,7 @@ class DeviceAPI < Grape::API
     params do
       requires :device, type: Hash do
         requires :name, type: String
+        requires :uid, type: String
         optional :token, type: String
         optional :last_user, type: String
         optional :is_returned, type: Boolean
@@ -21,12 +22,20 @@ class DeviceAPI < Grape::API
       device_params = params[:device]
       device = Device.find_by(name: device_params[:name])
       if device.nil?
-        device = Device.create(name: device_params[:name], token: device_params[:token], user_id: device_params[:last_user])
+        device = Device.create(name: device_params[:name], uid: device_params[:uid], token: device_params[:token], user_id: device_params[:last_user])
       else
-        device.update_attributes(token: device_params[:token], user_id: device_params[:last_user])
+        device.update_attributes(token: device_params[:token], uid: device_params[:uid], user_id: device_params[:last_user])
       end
       present status: 200 if device.save
     end
 
+    desc "Take device"
+    params do
+      requires :device_id, type: String
+      requires :user_id, type: String
+    end
+
+    post "take" do
+    end
   end
 end
