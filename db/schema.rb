d7b6701d71f548cd5092707b1951fa9383c349b3
latypova-alina <alinkin9696@mail.ru>
2017-10-27 18:12:55 +0300

    add to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171027135327) do
+ActiveRecord::Schema.define(version: 20171027150659) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20171027135327) do
     t.boolean "is_returned", default: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "uid"
   end
 
   create_table "users", force: :cascade do |t|
